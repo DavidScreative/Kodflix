7501ea54ba1c1528c20fdcc5921f56f4b3a82567
David Smith <creepingcats@btconnect.com>
2018-07-02 17:19:47 +0100

    Added new tv titles and covers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import logo from './movieimages/TheMistMovie.jpg';
 import './App.css';
+import Thewirecover from './tvshowimages/TheWire.jpg';
+import Breakingbadcover from './tvshowimages/BreakingBad.jpg';
+import Deathnotecover from './tvshowimages/DeathNote.jpg';
+import Fearthewalkingdeadcover from './tvshowimages/FearTheWalkingDead.jpg';
+import Gameofthronescover from './tvshowimages/GameOfThrones.jpg';
+import Blackmirrorcover from './tvshowimages/BlackMirror.jpg';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Dave's React V1.1</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className='image-cover-row'>
+            <div className='image-cover-row-item'>
+            <img src={Blackmirrorcover} alt="Black Mirror">
+            </img>
+              <h1>Black Mirror</h1>
+            </div>
+            <div className='image-cover-row-item'>
+            <img src={Breakingbadcover} alt="Breaking Bad">
+              </img>      
+              <h1>Breaking Bad</h1>
+            </div>
+            <div className='image-cover-row-item'>
+            <img src={Deathnotecover} alt="Death Note">
+              </img>      
+              <h1>Death Note</h1>
+            </div>
+          </div>
+            <div className='image-cover-row'>
+            <div className='image-cover-row-item'>
+            <img src={Gameofthronescover} alt="Game of Thrones">
+            </img>
+              <h1>Game of Thrones</h1>
+            </div>
+            <div className='image-cover-row-item'>
+            <img src={Fearthewalkingdeadcover} alt="Fear The Walking Dead">
+            </img>
+              <h1>Fear The Walking Dead</h1>
+            </div>
+             <div className='image-cover-row-item'>
+             <div className="Tvshow-cover">
+            <img src={Thewirecover} alt="The Wire">
+              </img>      
+              <h1>The Wire</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      
     );
-  }
+}
 }
 
 export default App;
