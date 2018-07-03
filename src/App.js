20834de3d16d31efe99c9007e64aeb16bb49552c
David Smith <creepingcats@btconnect.com>
2018-07-03 17:22:30 +0100

    Added additional css for the rollover state

--- src/App.js
+++ src/App.js
@@ -12,40 +12,72 @@ class App extends Component {
     return (
       <div className="App">
         <div className='image-cover-row'>
-            <div className='image-cover-row-item'>
+          <div className='image-cover-row-item'>
+
             <img src={Blackmirrorcover} alt="Black Mirror">
             </img>
-            </div>
-            <div className='image-cover-row-item'>
-            <img src={Breakingbadcover} alt="Breaking Bad">
-              </img>      
-            </div>
-            <div className='image-cover-row-item'>
-            <img src={Deathnotecover} alt="Death Note">
-              </img> 
+            <div className='image-cover-row-item-overlay'>
+              <h1>Black Mirror</h1>
+
             </div>
           </div>
-            <div className='image-cover-row'>
-            <div className='image-cover-row-item'>
-            <img src={Gameofthronescover} alt="Game of Thrones">
+
+
+          <div className='image-cover-row-item'>
+
+            <img src={Breakingbadcover} alt="Breaking Bad">
             </img>
-           </div>
-            <div className='image-cover-row-item'>
-            <img src={Fearthewalkingdeadcover} alt="Fear The Walking Dead">
+            <div className='image-cover-row-item-overlay'>
+              <h1>Breaking Bad</h1>
+
+            </div>
+          </div>
+
+
+          <div className='image-cover-row-item'>
+
+            <img src={Deathnotecover} alt="Death Note">
             </img>
-            </div>
-             <div className='image-cover-row-item'>
-             <div className="Tvshow-cover">
-            <img src={Thewirecover} alt="The Wire">
-              </img>      
-            </div>
+            <div className='image-cover-row-item-overlay'>
+              <h1>Death Note</h1>
+
             </div>
           </div>
         </div>
+        <div className='image-cover-row'>
+          <div className='image-cover-row-item'>
+            <img src={Gameofthronescover} alt="Game of Thrones">
+            </img>
+            <div className='image-cover-row-item-overlay'>
+              <h1>Game of Thrones</h1>
+            </div>
+          </div>
+          <div className='image-cover-row-item'>
+            <img src={Fearthewalkingdeadcover} alt="Fear The Walking Dead">
+            </img>
+            <div className='image-cover-row-item-overlay'>
+              <h1>Fear The Walking Dead</h1>
+            </div>
+          </div>
 
-      
+          <div className='image-cover-row-item'>
+            <img src={Thewirecover} alt="The Wire">
+            </img>
+            <div className='image-cover-row-item-overlay'>
+              <h1>The Wire</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
     );
-}
+  }
 }
 
 export default App;
